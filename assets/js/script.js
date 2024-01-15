d8c1b1f5517f671e2e57512c393c4263713ce47c
Melisa <andream_dearth@hotmail.com>
2024-01-15 23:23:25 +0000

    Appending the weather data from the current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,38 @@ $("#search-button").on("click", function (e) {
       //save my search input value to a variable
     const searchInput = $(".weather-search").val().trim();
 
+
 // Here we are building the URL we need to query the database
 const APIKey = "83b63f4667133ba418e4c4dff4868cf3";
 const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&appid=${APIKey}`;
 
 
 // We then created an Fetch call
+
 fetch(queryURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
+        const chosenCity = $("<h4>").text(`${data[0].name} ${dayjs().format("(DD/MM/YYYY)")}`); //Show the name of the city and the date with dayjs
 
-    const newQueryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=${APIKey}`
+    const weatherQueryUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=${APIKey}`
 
-        fetch(newQueryUrl)
+        fetch(weatherQueryUrl)
         .then(function (response) {
             return response.json()
         })
-        .then(function (data) {
-            console.log(data)
+        .then(function (weatherData) {
+            console.log(weatherData); // Show the weather details and add them to the 
+            const weatherToday = $("#today");
+            const weatherIcon = $("<img>").attr("src",`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`);
+            const weatherTemp = $("<p>").text(`Temp: ${weatherData.main.temp} °C`);
+            const weatherWind = $("<p>").text(`Wind: ${weatherData.wind.speed} KPH`);
+            const weatherHum = $("<p>").text(`Humidity: ${weatherData.main.humidity}%`);
+            weatherToday.append(chosenCity, weatherTemp, weatherWind, weatherHum);
+            chosenCity.append(weatherIcon);
         })
-    
+
     })
 });
